6fa8a108ec27fbdcdf62b8c25ea73988f2b02d32
Jesse Powers <jesse.l.powers@outlook.com>
2019-04-15 19:23:14 -0500

    home page

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return <div id ="nav-bar">
+    <div id="nav-bar-two">ACTION SPORTS AND GAMING</div>
+    <div id="menu">FACEBOOK | CALENDAR | BUY LIST | ARTISTS | LOG-IN</div>
+    <div id="address">6041 NE ANTIOCH RD GLADSTONE, MO 64119 | (816) 455-6319</div>
+    </div>
+}
+
+export default Navbar;
---
+++ src/components/ReviewsWidget.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ReviewsWidget() {
+    return <div id="reviews-widget"></div>
+}
+
+export default ReviewsWidget;
