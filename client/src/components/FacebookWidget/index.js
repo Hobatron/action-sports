e8fbe5c3c28d75de3e26501b4bf95547af70f9b5
Dylon <Dielonh@gmail.com>
2019-04-24 21:11:15 -0500

    Fixed conflicts on fb content

--- client/src/components/FacebookWidget/index.js
+++ client/src/components/FacebookWidget/index.js
@@ -5,8 +5,8 @@ import './FacebookWidget.css';
 function FacebookWidget() {
     return (
     <div id="facebook-widget">
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FActionSportsGames&tabs=timeline&width=500px&height=500px&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=598894817281877"
-            width="500px" height="500px"
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FActionSportsGames&tabs=timeline&width=350px&height=500px&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=598894817281877"
+            width="350px" height="500px"
             scrolling="no" frameborder="0" allowTransparency="true"
             allow="encrypted-media"></iframe>
     </div>
