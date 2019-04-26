e3e7c1562e7596f817fd2d380adbcba09f79033b
Jesse Powers <jesse.l.powers@outlook.com>
2019-04-26 13:20:33 -0500

    updating front page styles

--- client/src/components/EventsWidget/index.js
+++ client/src/components/EventsWidget/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './EventsWidget.css';
 
 const EventsWidget = () => {
-    return <div id="events-widget"></div>
+    return <div id="events-widget">
+    <br />
+        RSVP FOR UPCOMING <br />
+        EVENTS NOW!
+        <br /><br />
+        <button>RSVP</button>
+    </div>
 }
 
 export default EventsWidget;
