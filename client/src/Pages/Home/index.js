24ad973d880b3e225c3ccd780278576dc13af93b
aemejuobi <govallen@gmail.com>
2019-05-07 17:15:37 -0500

    added a search function, changed all admin login logout functionality to the admin page, changed Home index back to a stateless component

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { withAuth } from '@okta/okta-react';
+import React from 'react';
 
 import FacebookWidget from '../../components/FacebookWidget';
 import CalendarWidget from '../../components/CalendarWidget';
@@ -11,72 +9,33 @@ import './Home.css';
 
 
 
-export default withAuth(class Home extends Component {
-      constructor(props) {
-        super(props);
-        this.state = { authenticated: null };
-        this.checkAuthentication = this.checkAuthentication.bind(this);
-        this.login = this.login.bind(this);
-        this.logout = this.logout.bind(this);
-      }
-      
-      checkAuthentication = async() => {
-        const authenticated = await this.props.auth.isAuthenticated();
-        if (authenticated !== this.state.authenticated) {
-            this.setState({ authenticated });
-        }
-      }
-      
-      componentDidMount() {
-        this.checkAuthentication();
-      }
-      
-      componentDidUpdate() {
-        this.checkAuthentication();
-      }
-      
-      login() {
-        this.props.auth.login('/');
-      }
-      
-      logout() {
-        this.props.auth.logout('/');
-      }
+const Home = () => {
 
-      render(){
-            if (this.state.authenticated === null) return null;
-            
-            const button = this.state.authenticated ?
-            <button onClick={this.logout}>Logout</button> :
-            <button onClick={this.login}>Login</button>;
+      return (
+            <div className="container">
+                  <div className="row">
+                        <div className="col-sm-12 col-md-6 col-lg-4">
+                              <FacebookWidget />
+                        </div>
+                        <div className="col-sm-12 col-md-12 col-lg-8">
+                              <CarouselWidget />
+                        </div>
+                  </div>
+                  <div className="row justify-content-end">
+                        <div className="col-sm-12 col-md-6 col-lg-4">
+                              <EventsWidget />
+                        </div>
+                        <div className="col-sm-12 col-md-6 col-lg-4 align-self-end">
+                              <CalendarWidget />
+                        </div>
+                  </div>
+                  <div className="row justify-content-lg-center">
+                        <div className="col-sm-12 col-md-6 col-lg-4 align-self-center">
+                              <ReviewsWidget />
+                        </div>
+                  </div>
+            </div >
+      );
+};
 
-            return (
-                  <div className="container">
-                        <div className="row">
-                              <div className="col-sm-12 col-md-6 col-lg-4">
-                                    <FacebookWidget />
-                              </div>
-                              <div className="col-sm-12 col-md-12 col-lg-8">
-                                    <CarouselWidget />
-                              </div>
-                        </div>
-                        <div className="row justify-content-end">
-                              <div className="col-sm-12 col-md-6 col-lg-4">
-                                    <EventsWidget />
-                              </div>
-                              <div className="col-sm-12 col-md-6 col-lg-4 align-self-end">
-                                    <CalendarWidget />
-                              </div>
-                        </div>
-                        <div className="row justify-content-lg-center">
-                              <div className="col-sm-12 col-md-6 col-lg-4 align-self-center">
-                                    <ReviewsWidget />
-                              </div>
-                        </div>
-                        
-                        {button}
-                   </div >
-            );
-      }
-      
-});
+export default Home;
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -20,7 +20,7 @@ export class Navbar extends Component {
       <div id="as-nav-bar">
         <div className="container">
             <div id="title"><Link to="/">ACTION SPORTS AND GAMING</Link></div>
-            <div id="menu"><Link to="/calendar">EVENTS</Link> | <Link to="/buylist">BUY LIST</Link> | <Link to="/admin">LOG-IN</Link></div>
+            <div id="menu"><Link to="/calendar">EVENTS</Link> | <Link to="/buylist">BUY LIST</Link></div>
         </div>
         <div id="address">6041 NE ANTIOCH RD GLADSTONE, MO 64119 | (816) 455-6319</div>
 
