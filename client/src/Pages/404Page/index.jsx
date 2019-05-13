b65a1c84d97474a1c6d2cf08b98457661763cb58
mason ashlock <masonashlock@gmail.com>
2019-05-13 13:24:26 -0500

    finished 404 page

--- client/src/Pages/404Page/index.jsx
+++ client/src/Pages/404Page/index.jsx
@@ -1,21 +1,29 @@
 import React, { Component } from 'react'
-import { MDBRow, MDBContainer, MDBCol } from "mdbreact"
+import { MDBContainer } from "mdbreact"
 import "./index.css"
+import totallyLost from "../../components/images/404img.JPG"
+import logo from "../../components/images/ASLogo_Alpha.png"
+import { Link } from 'react-router-dom';
+
 
 export class page404 extends Component {
     render() {
         return (
             <MDBContainer className="wrapper">
-                <MDBRow className="header">
-
-                </MDBRow>
-                <MDBRow>
-                    This is a 404 page
-                </MDBRow>
-
-                <MDBRow bottom className="footer">
-
-                </MDBRow>
+                <div className="top text-center">
+                    <img src={logo} />
+                </div>
+                <div className="middle text-center">
+                    404
+                    <br />
+                    You seem...
+                    <img src={totallyLost} />
+                </div>
+                <Link className="link" to="/">
+                    <div className="text-center bottom">
+                        <div className="">Home</div>
+                    </div>
+                </Link>
 
             </MDBContainer>
         )
