5a88a5db1f102b433676279e4c3fd56a398853c7
mason ashlock <masonashlock@gmail.com>
2019-05-17 16:21:13 -0500

    conv for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose")
 const apiRoutes = require("./routes/apiRoutes");
-require("dotenv").config();
+// require("dotenv").config();
 
 
 const PORT = process.env.PORT || 3001;
@@ -19,14 +19,14 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
 //mongodb:localhost/action-sports-db
-mongoose.connect(process.env.DB_CONNECTION, {
+mongoose.connect("mongodb://asw:aswebdev1@ds137206.mlab.com:37206/heroku_2ztlncnk", {
   useNewUrlParser: true
 }, () => {
   console.log("Connected");
